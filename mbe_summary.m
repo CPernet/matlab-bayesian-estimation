dc0216e69d7318a9237a9916f801ed51b39a5728
Nils Winter <nils.winter1@gmail.com>
2016-04-12 17:04:13 +0100

    changed input so that function handles multiple time steps

--- mbe_summary.m
+++ mbe_summary.m
@@ -1,29 +1,29 @@
-function summary = mbe_summary(sampleVec,varargin)
+function summary = mbe_summary(mcmcChain,varargin)
 %% mbe_summary
 %   Computes summary statistics for one parameter of mcmc chain.
-%   Summary statistics include mean, median, mode, HDI and if a 
+%   Summary statistics include mean, median, mode, HDI and if a
 %   comparison value is specified the percentage of parameter data
-%   points above the threshold. This is useful i.e. for comparing 
+%   points above the threshold. This is useful i.e. for comparing
 %   groups and calculating the difference. The comparison value would
 %   then be 0.
 %
-% INPUT: 
-%   sampleVec
+% INPUT:
+%   mcmcChain
 %       mcmc chain of one parameter (vector)
 %   compVal
 %       comparison value
 %
-% OUTPUT:   
+% OUTPUT:
 %   summary
 %       structure with individual fields for statistics
 %
-% EXAMPLE:   
+% EXAMPLE:
 %   summary = mbe_summary(sampleVec,0);
 
 % Nils Winter (nils.winter1@gmail.com)
 % Johann-Wolfgang-Goethe University, Frankfurt
 % Created: 2016-03-15
-% Version: v1.00 (2016-03-15)
+% Version: v1.2 (2016-04-12)
 % Matlab 8.1.0.604 (R2013a) on PCWIN
 %-------------------------------------------------------------------------
 
@@ -31,18 +31,28 @@ if nargin > 1
     compVal = varargin{1};
 end
 
-summary.mean = mean(sampleVec);
-summary.median = median(sampleVec);
-% Get mode (for continous variables) by using ksdensity 
-[f,xi] = ksdensity(sampleVec);
-[~,I] = max(f);
-summary.mode = xi(I);
-hdiLim = HDIofMCMC(sampleVec,0.95);
-summary.HDIlow = hdiLim(1);
-summary.HDIhigh = hdiLim(2);
+names = fieldnames(mcmcChain);
 
-if exist('compVal','var')
-    summary.pcgtZero = (100*sum(sampleVec > compVal) ...
-        / length(sampleVec));
+
+for indPar = 1:numel(names)
+    summary.(names{indPar}).mean = squeeze(mean(mcmcChain.(names{indPar})));
+    summary.(names{indPar}).median = squeeze(median(mcmcChain.(names{indPar})));
+    % Get mode (for continous variables) by using ksdensity
+    for indTime = 1:size(mcmcChain.(names{1}),3)
+        for indChain = 1:size(mcmcChain.(names{1}),2)
+            [f,xi] = ksdensity(mcmcChain.(names{indPar})(:,indChain,indTime));
+            [~,I] = max(f);
+            summary.(names{indPar}).mode(indChain,indTime) = xi(I);
+            hdiLim = HDIofMCMC(mcmcChain.(names{indPar})(:,indChain,indTime),0.95);
+            summary.(names{indPar}).HDIlow(indChain,indTime) = hdiLim(1);
+            summary.(names{indPar}).HDIhigh(indChain,indTime) = hdiLim(2);
+            if exist('compVal','var')
+                summary.(names{indPar}).pcgtZero(indChain,indTime) = ...
+                    (100*sum(mcmcChain.(names{indPar})(:,indChain,indTime)...
+                    > compVal) / length(mcmcChain.(names{indPar})(:,indChain,indTime)));
+            end
+        end
+    end
 end
+
 end
