6200c53b3c8d96fab774175f86218dd49433a7de
Nils Winter <nils.winter1@gmail.com>
2016-04-12 16:18:27 +0100

    changed mcmcChain input and structure

--- mbe_diagMCMC.m
+++ mbe_diagMCMC.m
@@ -5,9 +5,8 @@ function mcmcInfo = mbe_diagMCMC(mcmcChain)
 %
 % INPUT:
 %   mcmcChain
-%       structure containing one structure for every chain.
-%       Fieldnames of substructures refer to parameter values.
-%       This is the default output of matjags.m.
+%       structure containing all parameters.
+%       Use mbe_restructChains.m to change structure of matjags output.
 %
 % OUTPUT:
 %   mcmcDiagInfo
@@ -18,51 +17,46 @@ function mcmcInfo = mbe_diagMCMC(mcmcChain)
 % Nils Winter (nils.winter1@gmail.com)
 % Johann-Wolfgang-Goethe University, Frankfurt
 % Created: 2016-03-16
-% Version: v1.1 (2016-03-16)
+% Version: v1.2(2016-04-12)
 % Matlab 8.1.0.604 (R2013a) on PCWIN
 %-------------------------------------------------------------------------
+
+%% Check if input is only one mcmc simulation (with multiple chains)
+if size(mcmcChain,3) > 1
+    t = uiwait(inputdlg('Which time step do you want to diagnose?'));
+    if t > size(mcmcChain,3
+        error('The time step you specified is not valid.')
+    end
+end
 
 %% Get parameters
 % make it a MxNxP-matrix (M=steps,N=parameters,P=chains)
 names = fieldnames(mcmcChain(1));
-paramNames = {};
-params = [];
-cnt = 1;
-for indChain = 1:numel(mcmcChain)
-    for indFields = 1:(numel(names)-1)  % -1 to skip 'deviance' parameter
-        for indParam = 1:size(mcmcChain(1).(names{indFields}),2)
-            params(:,cnt,indChain) = mcmcChain(indChain).(names{indFields})(:,indParam);
-            paramNames{cnt} = [names{indFields} num2str(indParam)];
-            cnt = cnt+1;
-        end
-    end
-    cnt = 1;
-end
 
 %% Loop through every parameter and create diagnostic plots
-for indParam = 1:numel(paramNames)
+for indParam = 1:numel(names)
     % Make one figure for every parameter
     figure('NumberTitle','Off','Color','w','Position',[100,50,800,600]);
     
     % Plot trace of parameter
     subplot(2,2,1);
-    mbe_tracePlot(squeeze(params(:,indParam,:)));
+    mbe_tracePlot(squeeze(mcmcChain.(names{indParam})));
     
     % Plot autocorrelation of parameters
     subplot(2,2,2);
-    mbe_acfPlot(squeeze(params(:,indParam,:)));  
+    mbe_acfPlot(squeeze(mcmcChain.(names{indParam})));  
     
     % Plot density
     subplot(2,2,4);
-    mbe_mcmcDensPlot(squeeze(params(:,indParam,:)))
+    mbe_mcmcDensPlot(squeeze(mcmcChain.(names{indParam})));
     
     % Plot evolution of shrinkage factor
     subplot(2,2,3);
-    mbe_gelmanPlot(squeeze(params(:,indParam,:)));
+    mbe_gelmanPlot(squeeze(mcmcChain.(names{indParam})));
     
     % Title
     dim = [.35 .7 .3 .3];
-    str = ['Chain Diagnostics for: ' paramNames{indParam}];
+    str = ['Chain Diagnostics for: ' names{indParam}];
     a = annotation('textbox',dim,'String',str,'FitBoxToText','on',...
         'EdgeColor','None');
     set(a,'FontSize',14,'FontWeight','bold');
