d479fe96e63b1c6edcd61b575fc9294d6febaee7
Nils Winter <nils.winter1@gmail.com>
2016-04-12 15:41:00 +0100

    changed data format and structure for mcmc chains

--- mbe_restructChains.m
+++ mbe_restructChains.m
@@ -1,49 +1,90 @@
 function mcmcChainOut = mbe_restructChains(mcmcChain)
 %% mbe_restructChains
-%   Restructures MCMC output in order to split up parameters with same
-%   name. This happens if JAGS and matjags monitor more than one parameter
-%   with the same name (i.e. "mu"). This function creates a structure with
-%   one field per parameter (i.e. "mu1","mu2").
+%   Restructures MCMC output (of matjags). Matjags creates multiple 
+%   structures when more than one chain is used, but stores parameters
+%   with the same name in the same variable. This function splits up 
+%   the parameters and creates one structure with all the monitored
+%   parameters. Parameters are stored as matrix with NxMxT, where N is
+%   the number of iterations per chain, M is the number of chains and T is
+%   the number of time steps (only for time course analysis).
 %
-% INPUT: 
+% INPUT:
 %   mcmcChain
-%       - default output created by matjags
+%       - structure containing the chains (default output created by 
+%           matjags)
+%   or mcmcChainTs
+%       - cell array containing one mcmcChain structure per time point
 %
-% OUTPUT:   
+% OUTPUT:
 %   mcmcChainOut
-%       - structure with one field for every parameter of the chain
+%       - structure with one field for every parameter
 %
-% EXAMPLE:   
+% EXAMPLE:
 
 % Nils Winter (nils.winter1@gmail.com)
 % Johann-Wolfgang-Goethe University, Frankfurt
 % Created: 2016-03-30
-% Version: v1.1
+% Version: v1.2 (2016-04-12)
 %------------------------------------------------------------------------
-%% Checking for correct input
-if size(mcmcChain{1},2) > 1
-    error('Currently only works on concatenated chains. Use mbe_concChain.m first.')
-end
 
-%% Get parameter names
-names = fieldnames(mcmcChain{1});
-paramNames = {};
-cnt = 1;
-for indFields = 1:(numel(names)-1)  % -1 to skip 'deviance' parameter
-        for indParam = 1:size(mcmcChain{1}.(names{indFields}),2)
-            paramNames{cnt} = [names{indFields} num2str(indParam)];
-            cnt = cnt+1;
-        end
-end
-
-%% Get parameters
-cnt = 1;
-for indTime = 1:numel(mcmcChain)
-    for indFields = 1:(numel(names)-1)  % -1 to skip 'deviance' parameter
-        for indParam = 1:size(mcmcChain{1}.(names{indFields}),2)
-            mcmcChainOut{indTime}.(paramNames{cnt}) = mcmcChain{indTime}.(names{indFields})(:,indParam);
+% Check if input is time series of multiple mcmc simulations or not
+% Input is ts (multiple mcmc simulations with multiple chains)
+if iscell(mcmcChain) 
+    % Get parameter names
+    names = fieldnames(mcmcChain{1});
+    pNames = {};
+    cnt = 1;
+    for indField = 1:(numel(names)-1)  % -1 to skip 'deviance' parameter
+        for indParam = 1:size(mcmcChain{1}(1).(names{indField}),2)
+            pNames{cnt} = [names{indField} num2str(indParam)];
             cnt = cnt+1;
         end
     end
+    
+    % Get parameters
     cnt = 1;
+    for indTime = 1:size(mcmcChain,2)
+        for indChain = 1:size(mcmcChain{1},2)
+            for indField = 1:(numel(names)-1)  % -1 to skip 'deviance' parameter
+                for indParam = 1:size(mcmcChain{1}(1).(names{indField}),2)
+                    mcmcChainOut.(pNames{cnt})(:,indChain,indTime) = mcmcChain{indTime}(indChain).(names{indField})(:,indParam);
+                    cnt = cnt+1;
+                end
+            end
+            cnt = 1;
+        end
+    end
+
+    
+% input is just one time step with one or more chains
+elseif isstruct(mcmcChain) 
+    % Get parameter names
+    names = fieldnames(mcmcChain(1));
+    pNames = {};
+    cnt = 1;
+    for indField = 1:(numel(names)-1)  % -1 to skip 'deviance' parameter
+        for indParam = 1:size(mcmcChain(1).(names{indField}),2)
+            pNames{cnt} = [names{indField} num2str(indParam)];
+            cnt = cnt+1;
+        end
+    end
+    
+    % Get parameters
+    cnt = 1;
+    for indChain = 1:size(mcmcChain,2)
+        for indField = 1:(numel(names)-1)  % -1 to skip 'deviance' parameter
+            for indParam = 1:size(mcmcChain(1).(names{indField}),2)
+                mcmcChainOut.(pNames{cnt})(:,indChain) = mcmcChain(indChain).(names{indField})(:,indParam);
+                cnt = cnt+1;
+            end
+        end
+        cnt = 1;
+    end
+    
+else
+    error(['Input must be either structure with mcmc chains (matjags output) '...
+        'or a cell array containing multiple mcmc chains (i.e. for ERP analysis).']);
 end
+
+
+
